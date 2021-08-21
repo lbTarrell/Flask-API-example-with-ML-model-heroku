9053f062551c759e15f436a46ec89e0f77c903e5
lbTarrell <62875506+lbTarrell@users.noreply.github.com>
2021-08-21 21:01:16 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -3,10 +3,7 @@ import numpy as np
 import model
 
 from flask import Flask, request, jsonify
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
+from app import app
 
 @app.route('/')
 def index():
